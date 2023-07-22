a10eee4a88981e21bdc55087c8bb4ea9cc72de99
jwlee <lemonade4813@gmail.com>
2023-07-22 15:48:31 +0900

    add bootstrap

--- src/MoviePage.js
+++ src/MoviePage.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-
+import { Card , Button, Container, Row, Col } from 'react-bootstrap';
 
 const MoviePage = () => {
 
@@ -43,20 +43,28 @@ const MoviePage = () => {
 
 
     return (
-        <div>
+        <Container className={"mt-3"}>
+            <Row>
             <h1>Movie</h1>
         
             {movies && movies.map(movie => (
-                <>
-                    <h1>{movie.title}</h1>
-                    <p>{movie.overview}</p>
-                    <p>{movie.vote_average}</p>
-                </>
+                <Col className='mt-3 mb-3'>
+                <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500"+movie.poster_path} />
+                <Card.Body>
+                  <Card.Title>{movie.title.slice(0,10)}</Card.Title>
+                  <Card.Text>
+                    {movie.overview.slice(0,80)}
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+              </Col>
             ))}
 
+            </Row>
 
-
-        </div>
+        </Container>
     );
 };
 
